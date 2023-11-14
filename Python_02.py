7fb736eff989bf5c8d1afb64c7cf4002b7ba1d99
Ashutosh Kumar Singh <ashutosh12505@gmail.com>
2023-11-15 01:16:17 +0530

    Add files via upload

---
+++ Python_02.py
@@ -0,0 +1,40 @@
+print (10/2)
+print (10%2)
+print (10//2)
+print (10*2)
+print (10**2)
+abc = "Ashutosh\nkumar\nsingh"
+print (abc)
+print (3*abc)
+print (abc*3)
+# abc = "ashutosh\n"
+# print(5*abc)
+# ab = "abcd"
+# print(ab + ab)
+# print("ab","ab")
+# pq = ab
+# print(pq)
+# sd = "abcd\n"
+# print(sd + sd)
+print("ab" "cd")
+#string value can be used separated by a space under"",but variable can't be used like this without + or , sign.
+word = "abcde fghij"
+print(word [6])
+print(word[7])
+print(word[-1])
+print(word[-8])
+print(word[20])
+print(word[-40])
+xyz = word[1] + word[3]+word[8]+word[2]
+print(xyz)
+#print(len(word))
+#wrong---print("The lenght of the string I wrote is" + len(word))
+#wrong---print("Length is"+len(word))
+
+#It is wrong because we cannot add(concatenate) string and int.
+#Correct syntax is written below.
+
+#print("The length of the string I wrote is",len(word))
+
+#print("The length of the string I wrote is\n" * len(word))
+#print("Ashutosh\n" * len(word))
