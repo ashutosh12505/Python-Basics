076a84654a4c6060acc21874bd28a7ca4d1e2cc3
Ashutosh Kumar Singh <ashutosh12505@gmail.com>
2023-11-14 11:32:25 +0530

    Add files via upload

---
+++ Python_01.py
@@ -0,0 +1,21 @@
+abc = 100.5
+print(abc)
+print("abc is",type(abc))
+abcd = 'Ashutosh'
+#print(abcd)
+#print("abcd is",type(abcd))
+abcde = 4+5j
+#print(abcde)
+#print("abcde is",type(abcde))
+abcdef = 500
+print(abcdef)
+print("abcdef is a",type(abcdef))
+#This is to add a comment.
+abcdefgh = 4//3
+print(abcdefgh)
+print("abcdefgh is",type(abcdefgh))
+#print(7**2)
+print(50%3)
+print(5/2)
+abc = "ashu\ntosh"
+print(abc)
