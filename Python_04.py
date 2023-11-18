3ef996f2bf2d1b23d514654a05cad7cad7ab02d0
Ashutosh Kumar Singh <ashutosh12505@gmail.com>
2023-11-18 18:42:33 +0530

    Add files via upload

---
+++ Python_04.py
@@ -0,0 +1,53 @@
+kalyani = ['a','b','c','d','e','f']
+
+#BUILT IN METHODS FOR LISTS
+
+#to find the length of list
+#print(len(kalyani))
+
+#to add an element at the end of the list
+#kalyani.append(2)
+#print(kalyani)
+
+#kalyani.append('2')
+#print(kalyani)
+
+#kalyani.append("g")
+#print(kalyani)
+
+#kalyani.append('g')
+#print(kalyani)
+
+#add an element at a specified index
+
+#kalyani.insert(4,'p')
+#print(kalyani)
+
+#to add 'K' between 'b' and 'c'
+#kalyani.insert(2,'K')
+#print(kalyani)
+
+#add a collection at the end of the list.
+#kalyani.extend(['p','q','r'])
+#print(kalyani)
+
+#adding a collection at a specified index
+#kalyani[3] = 1,2,3,4
+#kalyani[3:] = 1,2
+#kalyani[3:4] = ['p','q','r']
+#kalyani[3:3] = ['p','q','r'] >> ['a', 'b', 'c', 'p', 'q', 'r', 'd', 'e', 'f']
+print(kalyani)
+
+#REMOVAL OF ELEMENT FROM LIST
+
+kalyani = ['a','b','c','d','e','f']
+
+# kalyani[4:5] = [] #---for deletion of element at at a perticular index.
+# print(kalyani)
+
+#kalyani.pop()----for removing last element.
+#kalyani.pop(2)----for removing element at any particular index.
+#print(kalyani)
+
+#kalyani.remove('c')  ----first "c" will be removed.
+#print(kalyani).
